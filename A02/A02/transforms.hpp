8c0f60bd12e15ce7e269bd2e02350ff54e3612c1
Francesca <francesca1.grimaldi@mail.polimi.it>
2024-03-17 18:03:51 +0100

    placed pieces 7-11

--- A02/A02/transforms.hpp
+++ A02/A02/transforms.hpp
@@ -1,43 +1,43 @@
+#include <glm/gtx/transform2.hpp>
 
+void SetTransform(A02* A) {
+	/**************
+	 Using A->SetMatrix(i, M, focus) where:
+	 i -> index of the matrix to set (0 .. 14)
+	 M -> transform matrix to use
+	 focus -> true if you want to add the focus to that piece (only one please)
 
-void SetTransform(A02 *A) {
-/**************
- Using A->SetMatrix(i, M, focus) where:
- i -> index of the matrix to set (0 .. 14)
- M -> transform matrix to use
- focus -> true if you want to add the focus to that piece (only one please)
- 
- solve the puzzle. Piece 0 is alread in place, and the correct transform to put in place piece 1, is
- the identity matrix. Find the correct transformations to place the other 13 pieces (i = 2..14).
- The suggestion is to work on the pieces one at a time, setting the focus to the one you are working on.
- The piece with the focus, will be displayed in red. Moreover, its origin, and the orientation and size
- of its axis will be shown. This is necessary because not all the pieces have their center in the origin,
- but remember that the transformations, assumes that the center is in the origin. Some pieces (especially
- the ones with a high index) will require a combination of transformations, since the transform is not
- centered with the origin or the main axes.
- 
- WARNING!
- Since it is a C program, you can use for loops and functions if you think they can be helpful in your solution.
- However, please include all your code in this file, since it will be put in an automatic correction process
- for the final evaluation. Please also be cautious when using standard libraries and symbols, since they
- might not be available in all the development environments (especially, they might not be available
- in the final evaluation environment, preventing your code from compiling).
- This WARNING will be valid far ALL THE ASSIGNMENTs, but it will not be repeated in the following texts,
- so please remember these advices carefully!
- 
-***************/
+	 solve the puzzle. Piece 0 is alread in place, and the correct transform to put in place piece 1, is
+	 the identity matrix. Find the correct transformations to place the other 13 pieces (i = 2..14).
+	 The suggestion is to work on the pieces one at a time, setting the focus to the one you are working on.
+	 The piece with the focus, will be displayed in red. Moreover, its origin, and the orientation and size
+	 of its axis will be shown. This is necessary because not all the pieces have their center in the origin,
+	 but remember that the transformations, assumes that the center is in the origin. Some pieces (especially
+	 the ones with a high index) will require a combination of transformations, since the transform is not
+	 centered with the origin or the main axes.
+
+	 WARNING!
+	 Since it is a C program, you can use for loops and functions if you think they can be helpful in your solution.
+	 However, please include all your code in this file, since it will be put in an automatic correction process
+	 for the final evaluation. Please also be cautious when using standard libraries and symbols, since they
+	 might not be available in all the development environments (especially, they might not be available
+	 in the final evaluation environment, preventing your code from compiling).
+	 This WARNING will be valid far ALL THE ASSIGNMENTs, but it will not be repeated in the following texts,
+	 so please remember these advices carefully!
+
+	***************/
 	glm::mat4 M;
 
 	// this is the only one in place, and that should not be modified
 	M = glm::mat4(1.0f);
-	A->SetMatrix(0,  M, false);
+	A->SetMatrix(0, M, false);
 
 	// this is an example on how to highlight a piece and move it.
 	// to put it in place, just replace the following line with an identity matrix,
 	// that is: A->SetMatrix(0,  M, false);
 
 	M = glm::mat4(1.0f);			// to put this piece in places, uncomment this line and delete the two above
-	A->SetMatrix(1,  M, false);
+	A->SetMatrix(1, M, false);
 
 	// FROM THIS POINT ON, you have to find the correct transform to solve the puzzle.
 	// Piece 2 transformation: proportional scaling of factor 3 + translation
@@ -45,57 +45,80 @@ void SetTransform(A02 *A) {
 	glm::mat4 Sp2 = glm::scale(M, glm::vec3(3));
 	glm::mat4 T2 = glm::translate(M, glm::vec3(-6, 0, -3));
 	M = T2 * Sp2;
-	A->SetMatrix(2,  M, false);
-	
+	A->SetMatrix(2, M, false);
+
 	// Piece 3 transformation: rotation of 15° around z axis + translation
 	M = glm::mat4(1.0f);
-	glm::mat4 R3z = glm::rotate(M, glm::radians(-15.0f), glm::vec3(0,0,1));
+	glm::mat4 R3z = glm::rotate(M, glm::radians(-15.0f), glm::vec3(0, 0, 1));
 	glm::mat4 T3 = glm::translate(M, glm::vec3(0, 0, 1));
 	M = T3 * R3z;
-	A->SetMatrix(3,  M, false);
-	
+	A->SetMatrix(3, M, false);
+
 	// Piece 4 transformation: rotation of 180° around z axis + translation
 	M = glm::mat4(1.0f);
 	glm::mat4 R4z = glm::rotate(M, glm::radians(-180.0f), glm::vec3(0, 0, 1));
 	glm::mat4 T4 = glm::translate(M, glm::vec3(2, 0.1, 1));
 	M = T4 * R4z;
-	A->SetMatrix(4,  M, false);
-	
+	A->SetMatrix(4, M, false);
+
 	// Piece 5 transformation: translation
 	M = glm::mat4(1.0f);
 	glm::mat4 T5 = glm::translate(M, glm::vec3(4, 0, 4));
 	M = T5;
-	A->SetMatrix(5,  M, false);
-	
+	A->SetMatrix(5, M, false);
+
 	// Piece 6 transformation: non-proportional scaling + translation
 	M = glm::mat4(1.0f);
 	glm::mat4 S6 = glm::scale(M, glm::vec3(2, 1, 1.25));
 	glm::mat4 T6 = glm::translate(M, glm::vec3(0, 0, -1.25));
 	M = T6 * S6;
-	A->SetMatrix(6,  M, false);
-	
-	// Piece 7 transformation: 
+	A->SetMatrix(6, M, false);
+
+	// Piece 7 transformation: shear + translation
 	M = glm::mat4(1.0f);
-	A->SetMatrix(7,  M, true);
-	
+	glm::mat4 Sh7 = glm::shearZ3D(M, -0.5f, 0.0f);
+	glm::mat4 T7 = glm::translate(M, glm::vec3(2.5, 0, -1));
+	M = T7 * Sh7;
+	A->SetMatrix(7, M, false);
+
+	// Piece 8 transformation -> TODO: fix this to make it more precise
 	M = glm::mat4(1.0f);
-	A->SetMatrix(8,  M, false);
-	
+	glm::mat4 Ry8 = glm::rotate(M, glm::radians(83.0f), glm::vec3(0, 1, 0));
+	glm::mat4 ShZ8 = glm::shearZ3D(M, 0.65f, 0.0f);
+	glm::mat4 ShX8 = glm::shearX3D(M, 0.0f, 0.5f);
+	glm::mat4 S8 = glm::scale(M, glm::vec3(1.88, 1.0, 1.55));
+	glm::mat4 T8 = glm::translate(M, glm::vec3(0.98, 2.0, 0.95));
+	M = T8 * S8 * ShX8 * ShZ8 * Ry8;
+	A->SetMatrix(8, M, false);
+
+	// Piece 9 transformation: none, already in place
 	M = glm::mat4(1.0f);
-	A->SetMatrix(9,  M, false);
-	
+	A->SetMatrix(9, M, false);
+
+	// Piece 10 transformation: rotation of 90° around y + proportional scaling + translation
 	M = glm::mat4(1.0f);
+	glm::mat4 Ry10 = glm::rotate(M, glm::radians(90.0f), glm::vec3(0, 1, 0));
+	glm::mat4 Sp10 = glm::scale(M, glm::vec3(2));
+	glm::mat4 T10 = glm::translate(M, glm::vec3(3, 2, 2));
+	M = T10 * Sp10 * Ry10;
 	A->SetMatrix(10, M, false);
-	
+
+	// Piece 11 transformation: rotation of 135° around y axis + translation
 	M = glm::mat4(1.0f);
+	glm::mat4 Ry11 = glm::rotate(M, glm::radians(135.0f), glm::vec3(0, 1, 0));
+	glm::mat4 T11 = glm::translate(M, glm::vec3(1.3, 2, 2.538));
+	M = T11 * Ry11;
 	A->SetMatrix(11, M, false);
-	
+
+	// Piece 12 transformation: TOOD
 	M = glm::mat4(1.0f);
-	A->SetMatrix(12, M, false);
-	
+	A->SetMatrix(12, M, true);
+
+	// Piece 13 transformation: TODO
 	M = glm::mat4(1.0f);
 	A->SetMatrix(13, M, false);
-	
+
+	// Piece 14 transformation: TODO
 	M = glm::mat4(1.0f);
 	A->SetMatrix(14, M, false);
 }

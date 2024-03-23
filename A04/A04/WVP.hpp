b271dff6b21f1463a08cbc7b4ededdf86032ed66
Francesca <francesca1.grimaldi@mail.polimi.it>
2024-03-23 18:48:43 +0100

    completed assignment 4

--- A04/A04/WVP.hpp
+++ A04/A04/WVP.hpp
@@ -29,6 +29,16 @@ glm::mat4 MakeViewProjectionLookInDirection(glm::vec3 Pos, float Yaw, float Pitc
 	//	- Looking rool defined in formal parameter >Roll<
 	glm::mat4 M = glm::mat4(1.0f);
 
+	glm::mat4 Mprj = glm::perspective(FOVy, Ar, nearPlane, farPlane);
+	Mprj[1][1] *= -1;
+
+	glm::mat4 Mv = glm::rotate(M, -Roll, glm::vec3(0, 0, 1)) *
+				   glm::rotate(M, -Pitch, glm::vec3(1, 0, 0)) *
+				   glm::rotate(M, -Yaw, glm::vec3(0, 1, 0)) *
+				   glm::translate(M, glm::vec3(-Pos));
+
+	M = Mprj * Mv;
+
 	return M;
 }
 
@@ -48,6 +58,14 @@ glm::mat4 MakeViewProjectionLookAt(glm::vec3 Pos, glm::vec3 Target, glm::vec3 Up
 	//	- Looking rool defined in formal parameter >Roll<
 	glm::mat4 M = glm::mat4(1.0f);
 
+	glm::mat4 Mprj = glm::perspective(FOVy, Ar, nearPlane, farPlane);
+	Mprj[1][1] *= -1;
+
+	glm::mat4 Mv = glm::rotate(M, -Roll, glm::vec3(0, 0, 1)) *
+				   glm::lookAt(Pos, Target, Up);
+
+	M = Mprj * Mv;
+
 	return M;
 }
 
@@ -59,6 +77,11 @@ glm::mat4 MakeWorld(glm::vec3 Pos, float Yaw, float Pitch, float Roll) {
 	//	- Euler angle rotation roll defined in formal parameter >Roll<
 	//  - Scaling constant and equal to 1 (and not passed to the procedure)
 	glm::mat4 M = glm::mat4(1.0f);
+
+	M = glm::translate(M, Pos) *
+		glm::rotate(M, Yaw, glm::vec3(0, 1, 0)) *
+		glm::rotate(M, Pitch, glm::vec3(1, 0, 0)) *
+		glm::rotate(M, Roll, glm::vec3(0, 0, 1));	// no scaling
 
 	return M;
 }

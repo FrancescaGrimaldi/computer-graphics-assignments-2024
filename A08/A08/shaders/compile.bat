83371398b15839a85c96889fe356c37265658e4f
Francesca <francesca1.grimaldi@mail.polimi.it>
2024-08-18 19:32:06 +0200

    added batch file

---
+++ A08/A08/shaders/compile.bat
@@ -0,0 +1,13 @@
+glslc Shader.vert -o Vert.spv
+
+glslc TanShader.vert -o TanVert.spv
+
+glslc BlinnShader.frag -o BlinnFrag.spv
+
+glslc ToonShader.frag -o ToonFrag.spv
+
+glslc WardShader.frag -o WardFrag.spv
+
+glslc TextShader.frag -o TextFrag.spv
+
+glslc TextShader.vert -o TextVert.spv
